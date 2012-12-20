d0ffa3c032515c00fab79c9046850ba4b846e9cd
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2012-12-20 17:38:29 +0800

    added ScalaRe.scala

---
+++ trunk/benchmark/ScalaRe.scala
@@ -0,0 +1,19 @@
+import scalare.regex.pderiv.PDeriv._;
+
+object Main {
+  def main(args:Array[String]) = {
+    val usPat = {
+      val pSpace = PVar (-1, List(), PRE(Label(' ')))
+      val p1 = PVar (1,List(), PRE(Star(dot)))
+      val p2 = PVar (2,List(), PRE(Pair(char,char)))
+      val p3 = PVar (3,List(), PRE(repeatPair(digit)(5)))
+      val p4 = PVar (4,List(), PRE(Choice(Empty, Pair(Label('-'), repeatPair(digit)(4)))))
+      PPair(p1,PPair(pSpace,PPair(p2,PPair(pSpace, PPair(p3,p4)))))
+    }
+    val cp = compilePat(usPat)
+    val lines = scala.io.Source.fromFile(args(0), "utf-8").getLines
+    while (lines.hasNext) {
+      println(compiledGreedyPatMatch(cp)(lines.next()))
+    }
+  }
+}
